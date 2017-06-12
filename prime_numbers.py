5c540758d890d93f37051c42e923f2dacc964e4b
Chris-Maina <krismaish34@gmail.com>
2017-06-12 22:21:01 +0300

    Adding test for null input

--- prime_numbers.py
+++ prime_numbers.py
@@ -5,26 +5,27 @@ def printPrimeNumbers(number):
      Code to output prime numbers
      """
      listPrime=[]
-     try:
-          if isintance(number,basestring):
-               raise Exception
-          for prime in range (0,number+1):
-               if prime > 1:
-                     for num in range(2,prime):
-                           if(prime%num)==0:
-                               break
-                     else :
+     if isinstance(number,basestring):
+           raise Exception
+     if isinstance(number,integer):
+           raise Exception
+     if isinstance(number,float):
+           raise Exception
+     for prime in range (0,number+1):
+          if prime > 1:
+               for num in range(2,prime):
+                    if(prime%num)==0:
+                         break
+               else :
                           listPrime.append(prime)
-     except :
-          
-          print('Invalid output')
-     else:
-          return listPrime
+      
+           
+     return listPrime
      
      
      
 
-#print(printPrimeNumbers('one'))
+#print(printPrimeNumbers(10))
          
      
 
