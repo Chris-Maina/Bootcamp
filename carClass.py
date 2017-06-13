0aeae8322d0caa563094f42fc8d318afb255acac
Chris-Maina <krismaish34@gmail.com>
2017-06-13 16:15:13 +0300

    Car class that depicts type,model and name of car

--- carClass.py
+++ carClass.py
@@ -1,11 +1,43 @@
-class Car(object):
- def __init__(self,name = 'General',model= 'GM',type_car= 'honda'):
- 	self.name = name
- 	self.model = model
- 	self.type = type_car
+class Car():
+	def __init__(self,name = 'General',model= 'GM',car_type= 'honda',speed = 0):
+		self.name = name
+		self.model = model
+		self.car_type = car_type
+		self.speed = speed
+		
+		if name== 'Porshe' or name== 'Koenigsegg':
+			self.num_of_doors = 2
+		else:
+			self.num_of_doors = 4
 
- def car_instance(self):
- 	return self.name
+		if car_type == 'trailer':
+			self.num_of_wheels = 8
+		else:
+			self.num_of_wheels = 4
 
-#honda = Car('honda',None,None)
-#print honda.car_instance()
+	def is_saloon(self):
+		if self.type !=  'trailer':
+			self.car_type == 'saloon'
+			return True
+		else:
+			return False
+
+	def drive(self,moving_speed):
+		if moving_speed == 3:
+			self.speed = 1000
+		elif moving_speed == 7:
+			self.speed = 77
+
+		
+ return self
+ 
+
+	
+	
+	
+		
+	
+ 
+
+#gm = Car()
+#gm.default_car_name
