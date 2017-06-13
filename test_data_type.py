648016f09e6c6746bfc3cd3654ad9dc594391c65
Chris-Maina <krismaish34@gmail.com>
2017-06-13 11:28:26 +0300

    Added test for None input

--- test_data_type.py
+++ test_data_type.py
@@ -4,3 +4,6 @@ class DataTypeTestCase(unittest.TestCase):
   #test for string data supplied. 
   def test_str_type(self):
     self.assertEqual(6, data_type('andela'))
+  #test for null input/none
+  def test_none_type(self):
+    self.assertEqual('no value', data_type(None))
