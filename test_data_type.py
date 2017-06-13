b3d26db697789a531546920f2302c12dc97e0f61
Chris-Maina <krismaish34@gmail.com>
2017-06-13 11:57:14 +0300

    Added test for less than 100

--- test_data_type.py
+++ test_data_type.py
@@ -1,9 +1,15 @@
 import unittest
 from data_type import data_type
 class DataTypeTestCase(unittest.TestCase):
-  #test for string data supplied. 
+  #test for string  supplied. 
   def test_str_type(self):
     self.assertEqual(6, data_type('andela'))
-  #test for null input/none
+  #test for null input/none 
   def test_none_type(self):
     self.assertEqual('no value', data_type(None))
+  #test for boolean 
+  def test_small_booleans_type(self):
+    self.assertEqual(True,data_type(True))
+  #test for less than hundred data supplied
+  def test_small_integer_type(self):
+    self.assertEqual('less than 100',data_type(3))
