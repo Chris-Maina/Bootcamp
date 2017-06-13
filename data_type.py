f844e6130951d09c2f4beaba10dabd62895a27a5
Chris-Maina <krismaish34@gmail.com>
2017-06-13 11:43:33 +0300

    Check for Integer supplied

--- data_type.py
+++ data_type.py
@@ -12,9 +12,19 @@ def data_type(data):
 	#check for boolean argument
 	if isinstance(data,bool):
 		return data
+	#check for integer argument
+	if isinstance(data,int):
+		if data < 100 :
+			return "Less than 100"
+		if data == 100:
+			return "Equal to 100"
+		else:
+			return "More than 100"
+		
 
 
 
 #print(data_type('andela'))
 #print(data_type(None))
-print(data_type(True))
+#print(data_type(True))
+print(data_type(4034))
