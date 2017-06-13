1874937f2fd8fe791b411e484da29ac066a5e264
Chris-Maina <krismaish34@gmail.com>
2017-06-13 11:34:44 +0300

    Added check for boolean argument

--- data_type.py
+++ data_type.py
@@ -3,10 +3,18 @@ function data_type that compares and
 returns results based on argument supllied
 """
 def data_type(data):
+	#check for string argument
 	if isinstance(data,basestring):
 		return len(data)
+	#check for None argument
 	if data is None:
 		return "no value"
+	#check for boolean argument
+	if isinstance(data,bool):
+		return data
+
+
 
 #print(data_type('andela'))
 #print(data_type(None))
+print(data_type(True))
