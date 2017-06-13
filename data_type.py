2bfe3727b6a6ae02f804fa71d6c0f154529d86c7
Chris-Maina <krismaish34@gmail.com>
2017-06-13 11:27:56 +0300

    Added check for None input

--- data_type.py
+++ data_type.py
@@ -5,5 +5,8 @@ returns results based on argument supllied
 def data_type(data):
 	if isinstance(data,basestring):
 		return len(data)
+	if data is None:
+		return "no value"
 
-print(data_type('andela'))
+#print(data_type('andela'))
+#print(data_type(None))
